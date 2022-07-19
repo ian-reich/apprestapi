d154e25c649c5dbe3927ea52a16da1ce76efce07
Ian <ianwebikirda@gmail.com>
2022-07-19 20:35:55 +0700

    menambah controller file

---
+++ controller.js
@@ -0,0 +1,6 @@
+'use strict';
+var response = require('./res');
+var connection = require('./koneksi');
+exports.index = function(req, res){
+    response.ok('Aplikasi REST API Berjalan Sukses')
+};
