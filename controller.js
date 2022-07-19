0a62415514de57f1f0a1b6a09532ddb05fd71338
Ian <ianwebikirda@gmail.com>
2022-07-19 20:57:43 +0700

    update controller

--- controller.js
+++ controller.js
@@ -2,5 +2,5 @@
 var response = require('./res');
 var connection = require('./koneksi');
 exports.index = function(req, res){
-    response.ok('Aplikasi REST API Berjalan Sukses');
-}
+    response.ok('Aplikasi REST API Berjalan Sukses', res)
+};
